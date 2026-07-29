40c449c87619c56876b4d4720ca5d9fe7a5570fc
CRBroughton <crbroughton@posteo.uk>
2026-07-29 19:53:53 +0100

    feat(modules): :sparkles: add support for icons in which-key plugin

--- modules/neovim/plugins/which-key.lua
+++ modules/neovim/plugins/which-key.lua
@@ -1,6 +1,7 @@
 return {
     'folke/which-key.nvim',
     event = 'VeryLazy',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         local wk = require('which-key')
         wk.setup({ delay = 100 })
